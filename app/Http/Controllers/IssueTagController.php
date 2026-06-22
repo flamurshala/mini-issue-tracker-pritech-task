f0bfd5bce22d00fabfbe6e87d4cebbef8d0db028
Flamur Shala <flamur8600@gmail.com>
2026-06-23 00:06:08 +0200

    created controllers and form requests

---
+++ app/Http/Controllers/IssueTagController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IssueTagController extends Controller
+{
+    public function attach(Request $request)
+    {
+        //
+    }
+
+    public function detach(Request $request)
+    {
+        //
+    }
+}
