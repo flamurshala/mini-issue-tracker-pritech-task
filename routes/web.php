a1950f77c43dee1d4cfc2020d5aac37b12d52d59
Flamur Shala <flamur8600@gmail.com>
2026-06-23 00:18:08 +0200

    settng up routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\IssueController;
+use App\Http\Controllers\IssueTagController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('projects.index');
 });
+
+Route::resource('projects', ProjectController::class);
+Route::resource('issues', IssueController::class);
+
+Route::get('tags', [TagController::class, 'index'])->name('tags.index');
+Route::post('tags', [TagController::class, 'store'])->name('tags.store');
+
+Route::post('issues/{issue}/tags', [IssueTagController::class, 'attach'])->name('issues.tags.attach');
+Route::delete('issues/{issue}/tags/{tag}', [IssueTagController::class, 'detach'])->name('issues.tags.detach');
+
+Route::get('issues/{issue}/comments', [CommentController::class, 'index'])->name('issues.comments.index');
+Route::post('issues/{issue}/comments', [CommentController::class, 'store'])->name('issues.comments.store');
